28a52d71f66bfe8623058817eb8f0cc483f51360
Juno Chiu <junochiu@Junos-MacBook-Air.local>
2023-04-22 11:19:58 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  padding: 0em 8em;
+  background-image: linear-gradient(#53286F, #ffffff);
+  height: 100vh;
+  color: white;
+`
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <PageContainer>
+        <Navbar>
+
+        </Navbar> 
+      </PageContainer>
+    )
 }
 
 export default App;
